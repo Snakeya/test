b6538e4b6afa8a61252ad0144ad5e0757cd6461a
LAPTOP-REOS7BFD\hxx <1902340805@qq.com>
2023-06-16 21:21:02 +0800

    添加定时器外部时钟实验，验证PA0可用，PB12不可用，可能与引脚特定功能有关

--- STM32_demo/system/timer.c
+++ STM32_demo/system/timer.c
@@ -1,6 +1,6 @@
 #include "stm32f10x.h"
 #include "Serial.h"
-
+#define USE_EXTERNAL_CLOCK 1
 /*
         1.配置RCC,打开外设时钟
         2.选择时基单元的时钟源 //stm32f10x_tim.h,1073~1081
@@ -11,17 +11,36 @@
 */
 void timer_init(void)
 {
-    //1
+     //1
     RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2,ENABLE);//注意不同定时器挂载的总线区别
-    //2
+    #ifdef USE_EXTERNAL_CLOCK//使用外部时钟
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA,ENABLE);
+    GPIO_InitTypeDef GPIO_InitStructure;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;//上拉输入
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOA,&GPIO_InitStructure);
+    TIM_ETRClockMode2Config(TIM2, TIM_ExtTRGPSC_OFF, TIM_ExtTRGPolarity_NonInverted, 0x0F);
+    #else
     TIM_InternalClockConfig(TIM2);
+    #endif
+    //2
+    //TIM_InternalClockConfig(TIM2);
     //3
     TIM_TimeBaseInitTypeDef timebase_str;
     timebase_str.TIM_CounterMode = TIM_CounterMode_Up;//向上计数
     //ARR+PSC决定定时频率（时间），定时频率 = CK_CNT/(ARR+1) = CK_PSC/(PSC+1)/(ARR+1)
-    timebase_str.TIM_Period = 10000 - 1;//ARR，
-    timebase_str.TIM_Prescaler = 7200 - 1;//PSC，对72M进行7200分频，得到10k计数频率
-
+    if(USE_EXTERNAL_CLOCK)
+    {
+        timebase_str.TIM_Period = 10 - 1;//ARR，
+        timebase_str.TIM_Prescaler = 1 - 1;//PSC
+    } 
+    else
+    {
+        timebase_str.TIM_Period = 10000 - 1;//ARR，
+        timebase_str.TIM_Prescaler = 7200 - 1;//PSC，对72M进行7200分频，得到10k计数频率
+    }
+    
     timebase_str.TIM_ClockDivision = TIM_CKD_DIV1;//不管
     timebase_str.TIM_RepetitionCounter = 0;//高级定时器的参数，不需要给0
     TIM_TimeBaseInit(TIM2,&timebase_str);
