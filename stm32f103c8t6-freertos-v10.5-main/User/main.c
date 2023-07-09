721c6c4b5f937fb14065c24e9e7375854f98d7fc
LAPTOP-REOS7BFD\hxx <1902340805@qq.com>
2023-07-09 22:18:06 +0800

    任务删除实验

--- stm32f103c8t6-freertos-v10.5-main/User/main.c
+++ stm32f103c8t6-freertos-v10.5-main/User/main.c
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2023-07-08 21:40:20
- * @LastEditTime: 2023-07-09 21:57:22
+ * @LastEditTime: 2023-07-09 22:08:01
  * @LastEditors: LAPTOP-REOS7BFD
  * @Description: In User Settings Edit
  * @FilePath: \stm32f103c8t6-freertos-v10.5-main\User\main.c
@@ -13,6 +13,7 @@
 #include "portmacro.h"
 #include "Serial.h"
 
+//任务句柄，通过句柄操作任务，如删除
 static TaskHandle_t serial_task1_handle = NULL;
 static TaskHandle_t serial_task2_handle = NULL;
 
@@ -46,17 +47,30 @@ void serial_task1(void *arg)
 
 void serial_task2(void *arg)
 {
+	uint32_t i;
     while(1)
     {
         printf("2\r\n");
+        i++;
+		if(i == 1000)
+		{
+			vTaskDelete(serial_task1_handle);//通过句柄删除任务1
+		}
+		if(i == 1500)
+		{
+			vTaskDelete(NULL);//自删,传入NULL
+		}
+        //对于静态创建的任务，通过创建的时候返回的handle去删除
     }
 }
 
 void serial_task3(void *arg)
 {
+	
 	while(1)
 	{
 		printf("3\r\n");
+		
 	}
 }
 
@@ -68,7 +82,7 @@ int main(void)
 	/*
 		默认调度机制下：高优先级的任务优先执行，如果优先级没有主动放弃运行的话，低优先级的任务不会运行
 	*/
-	xTaskCreate(serial_task1,"task1",100,NULL,2,&serial_task1_handle);
+	xTaskCreate(serial_task1,"task1",100,NULL,1,&serial_task1_handle);
 	xTaskCreate(serial_task2,"task2",100,NULL,1,&serial_task2_handle);
 	/*
 		静态创建任务，需打开	configSUPPORT_STATIC_ALLOCATION
