861bfeddca287ddb91d031ac036f613879727d5f
LAPTOP-REOS7BFD\hxx <1902340805@qq.com>
2023-07-26 20:20:14 +0800

    other

--- STM32工程模板/User/main.c
+++ STM32工程模板/User/main.c
@@ -1,21 +1,23 @@
 /*
  * @Author: your name
  * @Date: 2023-07-25 13:51:37
- * @LastEditTime: 2023-07-25 19:37:12
+ * @LastEditTime: 2023-07-26 20:16:59
  * @LastEditors: LAPTOP-REOS7BFD
  * @Description: In User Settings Edit
  * @FilePath: \STM32工程模板\User\main.c
  */
 #include "stm32f10x.h"                  // Device header
 #include "Delay.h"
-
+#include "stopwatch.h"
 #include "IIC_OLED.h"
 #include "Serial.h"
 #include "mpu6050.h"
 #include "beep.h"
 #include "u8g2.h"
+#include "watchface.h"
+#include "key.h"
 
-mpu6050_data mpu_data;
+
 uint8_t u8g2_gpio_and_delay_stm32(U8X8_UNUSED u8x8_t *u8x8, U8X8_UNUSED uint8_t msg, U8X8_UNUSED uint8_t arg_int, U8X8_UNUSED void *arg_ptr)
 {
 	switch(msg)
@@ -44,76 +46,83 @@ uint8_t u8g2_gpio_and_delay_stm32(U8X8_UNUSED u8x8_t *u8x8, U8X8_UNUSED uint8_t 
 	}
 	return 1; // command processed successfully.
 }
-void draw(u8g2_t *u8g2)
-{
-	u8g2_ClearBuffer(u8g2); 
-    u8g2_SetFontMode(u8g2, 1); 
-    u8g2_SetFontDirection(u8g2, 0); 
-    u8g2_SetFont(u8g2, u8g2_font_inb24_mf);
-    u8g2_DrawStr(u8g2, 0, 20, "U");
-    
-    u8g2_SetFontDirection(u8g2, 1);
-    u8g2_SetFont(u8g2, u8g2_font_inb30_mn);
-    u8g2_DrawStr(u8g2, 21,8,"8");
-        
-    u8g2_SetFontDirection(u8g2, 0);
-    u8g2_SetFont(u8g2, u8g2_font_inb24_mf);
-    u8g2_DrawStr(u8g2, 51,30,"g");
-    u8g2_DrawStr(u8g2, 67,30,"\xb2");
-    
-    u8g2_DrawHLine(u8g2, 2, 35, 47);
-    u8g2_DrawHLine(u8g2, 3, 36, 47);
-    u8g2_DrawVLine(u8g2, 45, 32, 12);
-    u8g2_DrawVLine(u8g2, 46, 33, 12);
-  
-    u8g2_SetFont(u8g2, u8g2_font_4x6_tr);
-    u8g2_DrawStr(u8g2, 1,54,"github.com/olikraus/u8g2");
-//	u8g2_ClearDisplay(u8g2);
-	u8g2_SendBuffer(u8g2);
-	//Delayxms(1000);
-}
 
+u8g2_t u8g2;
+mpu6050_data mpu_data;
+extern Key_t key;
+extern stopwatch_t stopwatch1;
 int main(void)
 {
 	Serial_Init();
+    stopwatch_init();
     //OLED_Init();
     //OLED_I2C_Init();
 	printf("hello,world\r\n");
 	//OLED_ShowString(1,1,"hello");
     mpu6050_init();
     beep_init();
+    key_init();
+    RTC_Init();	
+    RTC_Set(2023,7,30,1,1,1);  //设置时间
     OLED12864_IoInit();
     
-    u8g2_t u8g2;
+    
     u8g2_Setup_ssd1306_i2c_128x64_noname_f(&u8g2, U8G2_R0, u8x8_byte_sw_i2c, u8g2_gpio_and_delay_stm32);
     u8g2_InitDisplay(&u8g2); 
     u8g2_SetPowerSave(&u8g2, 0);
     u8g2_ClearBuffer(&u8g2);
-    Delay_ms(1000);
-    u8g2_DrawLine(&u8g2, 0,0, 127, 63);
-	u8g2_DrawLine(&u8g2, 127,0, 0, 63);
     u8g2_SendBuffer(&u8g2);
-    draw(&u8g2);
 
+   
     
+    //watchface_ShowTime(&u8g2);
+    
+    //stopwatch_show(&u8g2);
+    
+//    u8g2_ClearBuffer(&u8g2);
+//    u8g2_SendBuffer(&u8g2);
 	while (1)
 	{
-        
+        key_get_value();
+        watchface_ShowTime(&u8g2);
+        if(key.key1 == 1 || key.key2 == 1 || key.key3 == 1 || key.key4 == 1)
+        {
+           
+            while(1)
+            {
+                key_get_value();
+                //printf("%d\r\n", key.key2);
+                printf("key2 = %d,state = %d\r\n",key.key2,stopwatch1.state );
+                if(key.key2 == 1 && (stopwatch1.state == SUSPEND))
+                {
+                    stopwatch_start();
+                    key.key2 = 0;
+                }
+                if(key.key2 == 1 && (stopwatch1.state == RUNING))
+                {
+                    stopwatch_suspend();
+                    key.key2 = 0;
+                }
+                if(key.key1 == 1)
+                {
+                    stopwatch_close();
+                    key.key1  = 0;
+                    break;
+                }
+                stopwatch_show(&u8g2);
+            }
+            
+        }
         MPU6050_GetData(&mpu_data);
-/*         OLED_ShowSignedNum(2, 1, mpu_data.AccX, 5);
-        OLED_ShowSignedNum(3, 1, mpu_data.AccY, 5);
-        OLED_ShowSignedNum(4, 1, mpu_data.AccZ, 5);
-        OLED_ShowSignedNum(2, 8, mpu_data.GyroX, 5);
-        OLED_ShowSignedNum(3, 8, mpu_data.GyroY, 5);
-        OLED_ShowSignedNum(4, 8, mpu_data.GyroZ, 5); */
         if(mpu_data.AccX > 100 || mpu_data.AccX < 0)
         {
             //OLED_Close();
            // beep_on(100);
+           u8g2_SetPowerSave(&u8g2, 1);
         }
         else
         {
-            //OLED_Open();
+            u8g2_SetPowerSave(&u8g2,0);
         }
 		
 	}
---
+++ STM32工程模板/system/iic.c
@@ -0,0 +1,192 @@
+#include "stm32f10x.h"
+#include "Delay.h"
+
+/*
+    写配置寄存器，读数据寄存器
+*/
+
+/*
+    两根通信线：
+        时钟，提供同步时序，从机只能读取时钟线
+        数据，一根，同一时刻只能收或发 ==》半双工，从机只能在主机发送读取从机的命令后（或从机应答时），才能短暂控制数据线
+    同步时序，半双工
+    带数据应答
+    支持总线挂载多设备（一主多从，多主多从）
+*/
+
+/*
+    硬件电路：ppt128
+        scl和sda均配置为开漏输出,并各加一个上拉电阻（4.7k）,总线空闲时，scl和sda由上拉电阻拉为高电平
+*/
+
+/*
+    时序基本单元：
+        start:scl高电平期间，sda从高电平切换到低电平
+        
+
+        发送一个字节：
+            scl低电平期间，主机将数据位依次放到sda上（高位先行）
+            释放scl,scl由低到高，从机在scl高电平期间读取sda电平（scl高电平期间，sda电平不允许变化）
+            循环8次
+        接收一个字节：
+            scl低电平期间，从机将数据位依次放到sda上（高位先行）
+            释放scl,scl由低到高，主机在scl高电平期间读取sda电平（scl高电平期间，sda电平不允许变化）
+            循环8次
+            主机接收前，需要释放sda
+
+        发送一个应答：时序同发送一个字节
+            主机在接收完一个字节后，在下一个时钟发送 一位数据，0表示应答，1表示非应答
+        接收一个应答：时序同接收一个字节
+            从机在发送完一个字节后,在下一个时钟接收 一位数据,判断从机是否应答，0表示应答,1表示非应答
+            主机接收前，需要释放sda
+
+        end:scl高电平期间，sda从低电平切换到高电平
+            开始前和结束后，即空闲时，scl和sda由上拉电阻拉为高电平
+
+*/
+
+/*
+    指定地址写：
+        start
+        确定通信设备
+            主机发送一个 地址：出厂时厂商分配的7位（最多）or 十位地址，参考芯片手册，不同型号芯片地址不同
+            如果总线挂载相同芯片
+                可变地址：地址最后几位可以通过电路改变
+        发送从机地址：7位
+        发送读写位：1位 0
+            0：写入
+            1：读取
+        接收一个应答位：
+        发送从机寄存器地址：
+        接收应答：
+        发送写入的数据：
+        end
+
+    当前地址读：
+        用的不多
+
+    指定地址读：
+        指定地址：调用指定地址写的前部分时序，获取读的地址
+            start
+            确定通信设备
+                主机发送一个 地址：出厂时厂商分配的7位（最多）or 十位地址，参考芯片手册，不同型号芯片地址不同
+                如果总线挂载相同芯片
+                    可变地址：地址最后几位可以通过电路改变
+            发送从机地址：7位
+            发送读写位：1位 0
+                0：写入
+                1：读取
+            接收一个应答位：
+            发送从机寄存器地址：
+            接收应答：
+        当前地址读：
+            start
+            发送从机地址
+            发送读写位
+            主机接收一个字节 
+            end      
+*/
+
+/*
+    由于iic开漏+上拉电阻的电路结构，会使通信线高电平驱动能力较弱，表现为：通信线由低电平变为
+    高电平时，上升沿时间较长，会限制iic的最大通信速度
+*/
+void soft_iic_write_scl(uint8_t bitvalue)
+{
+    GPIO_WriteBit(GPIOB,GPIO_Pin_10,(BitAction)bitvalue);
+    Delay_us(10); 
+}
+
+void soft_iic_write_sda(uint8_t bitvalue)
+{
+    GPIO_WriteBit(GPIOB,GPIO_Pin_11,(BitAction)bitvalue);
+    Delay_us(10);
+}
+
+uint8_t soft_iic_read_sda(void)
+{
+    uint8_t bit_value;
+    bit_value = GPIO_ReadInputDataBit(GPIOB,GPIO_Pin_11);
+    Delay_us(10);
+    return bit_value;
+}
+
+void soft_iic_init(void)
+{
+    GPIO_InitTypeDef GPIO_InitStructure;
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10 | GPIO_Pin_11;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_OD;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOB, &GPIO_InitStructure);
+
+    GPIO_ResetBits(GPIOB, GPIO_Pin_10 | GPIO_Pin_11);//拉高
+
+}
+
+void soft_iic_start(void)
+{
+    soft_iic_write_scl(1);
+    soft_iic_write_sda(1);
+    soft_iic_write_sda(0);
+    soft_iic_write_scl(0);
+}
+
+void soft_iic_stop(void)
+{
+    soft_iic_write_sda(0);
+    soft_iic_write_scl(1);
+    soft_iic_write_sda(1);
+}
+
+void soft_iic_sendbyte(uint8_t byte)
+{
+    uint8_t i;
+    for(i=0;i<8;i++)
+    {
+        soft_iic_write_sda((0x80 >> i) & byte);
+        soft_iic_write_scl(1);
+        soft_iic_write_scl(0);
+    } 
+}
+
+uint8_t soft_iic_recv_byte(void)
+{
+    uint8_t i;
+    uint8_t byte = 0x00;
+    soft_iic_write_sda(1);
+    for(i = 0;i < 8;i++)
+    {
+        soft_iic_write_scl(1);
+        if(soft_iic_read_sda())
+        {
+            byte |= (0x80 >> i);
+        }
+        soft_iic_write_scl(0);
+    }
+    return byte;
+}
+
+void soft_iic_send_ack(uint8_t ack)
+{
+    soft_iic_write_sda(ack);
+    soft_iic_write_scl(1);
+    soft_iic_write_scl(0);
+}
+
+uint8_t soft_iic_recv_ack(void)
+{
+    uint8_t ackbit;
+    soft_iic_write_sda(1);
+    soft_iic_write_scl(1);
+    ackbit = soft_iic_read_sda();
+    soft_iic_write_scl(0);
+    return ackbit;
+}
+    
+
+
+void hardware_iic_init(void)
+{
+
+}
