0eb2a0c61fc728c1ad823825c2656f6e693b94d8
LAPTOP-REOS7BFD\hxx <1902340805@qq.com>
2023-08-03 16:54:59 +0800

    弱函数测试

---
+++ 3弱函数/test.c
@@ -0,0 +1,15 @@
+/*
+ * @Author: your name
+ * @Date: 2023-08-03 16:48:28
+ * @LastEditTime: 2023-08-03 16:50:52
+ * @LastEditors: LAPTOP-REOS7BFD
+ * @Description: In User Settings Edit
+ * @FilePath: \C_test\3弱函数\test.c
+ */
+#include <stdio.h>
+
+
+
+void foo() {
+    printf("我是强函数 foo\n");
+}
---
+++ 3弱函数/test.h
@@ -0,0 +1,14 @@
+/*
+ * @Author: your name
+ * @Date: 2023-08-03 16:49:56
+ * @LastEditTime: 2023-08-03 16:50:39
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \C_test\3弱函数\test.h
+ */
+#ifndef __TEST_H__
+#define __TEST_H__
+
+void foo();
+
+#endif
